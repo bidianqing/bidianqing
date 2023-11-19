2a02912a047cfc14177ae4fc8961761799307bcc
bidianqing <bidianqing@qq.com>
2023-11-19 20:28:33 +0800

    添加项目文件。

---
+++ bidianqing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bidianqing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
